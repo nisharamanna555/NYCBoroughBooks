27c74d04851716875f8e2fffa1d4b46e7314d99d
nisharamanna555 <nisha.ramanna.20@gmail.com>
2023-12-03 16:33:38 -0500

    updating pinform.js

--- map-app/src/Map.js
+++ map-app/src/Map.js
@@ -14,7 +14,7 @@ const Map = ({ pins }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {pins.map((pin) => (
-        pin.location && (
+        pin.location && pin.location.coordinates && (
           <Marker key={pin._id} position={pin.location.coordinates}>
             <Popup>{pin.caption}</Popup>
           </Marker>
--- map-app/src/PinForm.js
+++ map-app/src/PinForm.js
@@ -1,23 +1,37 @@
 // src/PinForm.js
 import React, { useState } from 'react';
-
+import Map from './Map';
 const PinForm = ({ addPin }) => {
   const [caption, setCaption] = useState('');
   const [photo, setPhoto] = useState(null);
+  const [selectedLocation, setSelectedLocation] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Check if a location is selected
+    if (!selectedLocation) {
+      alert('Please select a location on the map');
+      return;
+    }
 
     // TODO: Implement photo upload logic (e.g., using FormData)
 
     const newPin = {
       caption,
       // photo: URL to the uploaded photo
-      location: [/* latitude */, /* longitude */], // Get the selected location from the map
+      location: [selectedLocation.lat, selectedLocation.lng], // Get the selected location from the map
     };
 
     // Call the addPin function to send the new pin to the backend
     addPin(newPin);
+  };
+  // Callback function to receive selected location from the map
+  const handleMapClick = (e) => {
+    setSelectedLocation({
+      lat: e.latlng.lat,
+      lng: e.latlng.lng,
+    });
   };
 
   return (
@@ -31,6 +45,8 @@ const PinForm = ({ addPin }) => {
         <input type="file" onChange={(e) => setPhoto(e.target.files[0])} />
       </label>
       {/* TODO: Add a map component for location selection */}
+      {/* <Map onClick={handleMapClick} selectedLocation={selectedLocation} /> */}
+
       <button type="submit">Add Pin</button>
     </form>
   );
