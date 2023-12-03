5f83127370457de94f0262f5f2d32fef0742827d
Chenny Yun <sy3031@nyu.edu>
2023-12-03 17:12:18 -0500

    Restrict view to NYC

--- map-app/src/Map.js
+++ map-app/src/Map.js
@@ -7,8 +7,15 @@ const Map = ({ pins }) => {
   const center = [40.7128, -74.0060]; // Coordinates for New York City
   console.log('Pins:', pins);
 
+  const bounds = [
+    [40.477399, -74.259090], // Southwest coordinates
+    [40.917577, -73.700272]  // Northeast coordinates
+  ];
+  
+
   return (
-    <MapContainer center={center} zoom={13} style={{ height: '500px', width: '100%' }}>
+    <MapContainer center={center} zoom={13} style={{ height: '500px', width: '100%' }} 
+    maxBounds={bounds} maxBoundsViscosity={1.0} minZoom = {12}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
