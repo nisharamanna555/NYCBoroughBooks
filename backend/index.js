3df22662731b2dd74a22f2da455c94308d758248
nisharamanna555 <nisha.ramanna.20@gmail.com>
2024-09-09 14:46:41 -0400

    Updating mongodb url

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
 const Pin = require('./models/pin')
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,11 +14,15 @@ app.use(cors())
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://nisharamanna20:Lakshmi5@cluster0.nioivw1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch((error) => console.error('Error connecting to MongoDB:', error));
 
-// Check for MongoDB connection success
-mongoose.connection.on('connected', () => {
-  console.log('Connected to MongoDB');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Add a pin
